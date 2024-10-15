32a2687db1678d8074d124f79a63f6f09b7deafd
dmitriy <kosheld@inbox.ru>
2024-10-15 21:37:36 +0300

    fix url

--- webApp/keyboard/inline_app.py
+++ webApp/keyboard/inline_app.py
@@ -3,7 +3,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def inline(tg_id, tg_nickname, phone_number):
-    url = f"https://ваш_адрес_приложения?phone_number={phone_number}&tg_id={tg_id}&tg_nickname={tg_nickname}"
+    url = f"https://dariedufront.vercel.app?phone_number={phone_number}&tg_id={tg_id}&tg_nickname={tg_nickname}"
     builder = InlineKeyboardBuilder()
 
     builder.row(types.InlineKeyboardButton(text="dari edu", web_app=types.WebAppInfo
