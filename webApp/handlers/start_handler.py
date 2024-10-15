d5f285ce6e6dbd54d481f19c2a5dd74618ba859c
dmitriy <kosheld@inbox.ru>
2024-10-15 20:54:11 +0300

    add user verification in the database. transferring data to the application.

--- webApp/handlers/start_handler.py
+++ webApp/handlers/start_handler.py
@@ -1,23 +1,33 @@
 from aiogram import Router, types, F
 from aiogram.filters import Command
 
-from webApp.keyboard.inline_app import inline
+from webApp.handlers.request_django import get_user_request
+from webApp.keyboard.inline_app import inline, inline_app
 from webApp.keyboard.inline_registration import inlines
 
 router = Router()
 
 @router.message(Command(commands=["start"]))
 async def start(message: types.Message):
-    await message.reply("Для регистрации вам необходимо поделиться с приложением своим номером телефона",
-                        reply_markup=inlines())
+    tg_id = message.from_user.id
+    tg_nickname = f"@{message.from_user.username}"
+    requests = get_user_request(tg_id)
+    if requests:
+        await message.reply("Добро пожаловать!", reply_markup=inline_app(tg_id, tg_nickname))
+    else:
+        await message.reply("Для регистрации вам необходимо поделиться с приложением своим номером телефона",
+                            reply_markup=inlines())
+
 
 @router.message(F.contact)
 async def contact_received(message: types.Message):
     tg_id = message.from_user.id
+    tg_nickname = f"@{message.from_user.username}"
     if message.contact:
         phone_number = message.contact.phone_number
         await message.answer(f"Спасибо!", reply_markup=types.ReplyKeyboardRemove())
-        await message.answer("Теперь нажмите на кнопку для перехода в приложение", reply_markup=inline())
+        await message.answer("Теперь нажмите на кнопку для перехода в приложение",
+                             reply_markup=inline(tg_id, tg_nickname, phone_number))
 
 @router.message(F.text == 'не делиться')
 async def text_received(message: types.Message):
--- webApp/keyboard/inline_app.py
+++ webApp/keyboard/inline_app.py
@@ -2,10 +2,21 @@ from aiogram import types
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def inline():
+def inline(tg_id, tg_nickname, phone_number):
+    url = f"https://ваш_адрес_приложения?phone_number={phone_number}&tg_id={tg_id}&tg_nickname={tg_nickname}"
     builder = InlineKeyboardBuilder()
 
     builder.row(types.InlineKeyboardButton(text="dari edu", web_app=types.WebAppInfo
-                (url="https://github.com/dariedu/botWebApp")))
+                (url=url)))
+
+    return builder.as_markup()
+
+
+def inline_app(tg_id, tg_nickname):
+    url = f"https://ваш_адрес_приложения?tg_id={tg_id}&tg_nickname={tg_nickname}"
+    builder = InlineKeyboardBuilder()
+
+    builder.row(types.InlineKeyboardButton(text="dari edu", web_app=types.WebAppInfo
+                (url=url)))
 
     return builder.as_markup()
