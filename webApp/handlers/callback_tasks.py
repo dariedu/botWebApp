fec6734e6776b6e991e94cbb74a7d851756b2bdc
dmitriy <kosheld@inbox.ru>
2024-11-02 15:14:26 +0300

    fix

--- webApp/handlers/callback_tasks.py
+++ webApp/handlers/callback_tasks.py
@@ -11,7 +11,7 @@ router_task = Router()
 @router_task.callback_query()
 async def handling_task_call(call: types.CallbackQuery, bot: Bot):
     if call.data == 'accept_task':
-        await call.message.edit_text("Подтверждено task")
+        await call.message.edit_text("Подтверждено")
     elif call.data.startswith('refuse_task'):
         data_str = call.data.split(':', 1)[1]
         try:
